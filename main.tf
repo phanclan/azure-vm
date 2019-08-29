c0a1b6f15298a8254bc3f8853c24cec904b10d26
Peter Phan <pphan@hashicorp.com>
2019-08-29 11:47:10 -0700

    removed module version

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ variable "admin_password" {
 module "windowsserver" {
   source              = "Azure/compute/azurerm"
   # version             = "1.1.5"
-  location            = "${var.location}"
+  location            = "var.location
   resource_group_name = "${var.windows_dns_prefix}-rc"
   vm_hostname         = "pwc-ptfe"
-  admin_password      = "${var.admin_password}"
+  admin_password      = {var.admin_password
   vm_os_simple        = "WindowsServer"
   public_ip_dns       = ["${var.windows_dns_prefix}"]
   vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
