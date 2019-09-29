dad8af0ebb557fee7f5891e3edf1043ed30b2fbb
Peter Phan <pphan@hashicorp.com>
2019-09-28 18:08:50 -0700

    removed linux vm

--- main.tf
+++ main.tf
@@ -29,17 +29,6 @@ module "windowsserver" {
   vnet_subnet_id      = module.network.vnet_subnets[0]
 }
 
-module "linuxservers" {
-  source              = "Azure/compute/azurerm"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
-  location            = var.location
-  vm_hostname         = "phan-linux-ptfe"
-  vm_os_simple        = "UbuntuServer"
-  ssh_key             = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjOXiqjoBMlfCBvmG6BcUGPv1q+YqNYLHlm6X18Frue+Yf2zG/56pMWtSoPbHKB+Nul0VNpANuOyt3qsEU+HtZz9MMTBiWL6kGH6S0saLMp7EpcZaib/Qxfkl1By6JnOwr6w7eW+XE4TXHRdBKaRWW4J52KdhlPXAeMFeSDL3qnZWaP7tIyKTQzdDXu0rSJIBpcYCVCQ5BkshWNvoVpDH0dH9r4ayLrzgnNzQHyqVFASU3DxqIAqrC3JflAz1aUWiwXhDJeZU3w6eDWvYxOAm+Z2vP5oiX/pqbYMlCUlPrsU5+6828kDQ5uQaZiCnSi2Bj3BDqpJngiVvyicJgvhW9 pephan@Mac-mini.local"
-  public_ip_dns       = ["phan-linsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-}
-
 module "network" {
   source              = "Azure/network/azurerm"
   version             = "2.0.0"
