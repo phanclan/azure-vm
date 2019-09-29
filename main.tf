5086d08cda2cb1721040adafa46b0b1081eff070
Peter Phan <pphan@hashicorp.com>
2019-09-28 18:00:40 -0700

    added linux vm

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ module "linuxservers" {
   location            = var.location
   vm_hostname         = "phan-linux-ptfe"
   vm_os_simple        = "UbuntuServer"
+  ssh_key             = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjOXiqjoBMlfCBvmG6BcUGPv1q+YqNYLHlm6X18Frue+Yf2zG/56pMWtSoPbHKB+Nul0VNpANuOyt3qsEU+HtZz9MMTBiWL6kGH6S0saLMp7EpcZaib/Qxfkl1By6JnOwr6w7eW+XE4TXHRdBKaRWW4J52KdhlPXAeMFeSDL3qnZWaP7tIyKTQzdDXu0rSJIBpcYCVCQ5BkshWNvoVpDH0dH9r4ayLrzgnNzQHyqVFASU3DxqIAqrC3JflAz1aUWiwXhDJeZU3w6eDWvYxOAm+Z2vP5oiX/pqbYMlCUlPrsU5+6828kDQ5uQaZiCnSi2Bj3BDqpJngiVvyicJgvhW9 pephan@Mac-mini.local"
   public_ip_dns       = ["phan-linsimplevmips"] // change to a unique name per datacenter region
   vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
 }
