7b17badc91468fa30d9b2f8b815d28e96cff0308
Peter Phan <pphan@hashicorp.com>
2019-09-28 17:21:12 -0700

    change module version

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ variable "admin_password" {
 
 module "windowsserver" {
   source              = "Azure/compute/azurerm"
-  version             = "1.3.0"
+  # version             = "1.3.0"
   location            = var.location
   resource_group_name = "${var.windows_dns_prefix}-rc"
   vm_os_simple        = "WindowsServer"
